08de249a21906309cabbebaa0f14c8af950c450f
Alekh <alekhkumar777@gmail.com>
2021-06-18 22:15:48 +0530

    Add box, enemy and player

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 let myCanvas = document.querySelector('#myCanvas');
 let pen = myCanvas.getContext("2d");
+
+class Box{
+    constructor(size, color){
+        this.size = size;
+        this.color = color;
+        this.x = 0;
+        this.y = 0;
+    }
+}
+
+class Player extends Box{
+    constructor(){
+        super(50, 'blue')
+        this.x = 0;
+        this.y = 225;
+    }
+}
+
+class Enemy extends Box{
+    constructor(){
+        super(50, 'red')
+    }
+}
